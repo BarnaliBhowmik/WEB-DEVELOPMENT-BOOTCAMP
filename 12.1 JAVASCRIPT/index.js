106236f919d5ca5bbb45a28fe3d4ca36535b6b15
BarnaliBhowmik <cocoospeak@gmail.com>
2025-03-11 06:17:27 +0530

    first commit

---
+++ 12.1 JAVASCRIPT/index.js
@@ -0,0 +1,25 @@
+// 1. Create a var that stores the name that user enters via prompt
+
+var name = prompt("What is your name?");
+
+// 2. To find remaining characters in the character limit
+
+var tweet = prompt("Compose a tweet-");
+alert("You have written " + tweet.length + " characters, you have " + (140 - tweetCount) + " characters remaining.");
+
+// 3. To create pop-ups that appear one after the other
+
+alert("Hello !");
+alert("WORLD !!");
+
+// 2. Capitalize the first letter of their name.
+
+// a. Isolate the first char
+
+// b. Turn the first char to upper case
+
+// c. Isolate the rest of the name
+
+// d. Concatenate the first char with the rest of the char
+
+// 3. We 
