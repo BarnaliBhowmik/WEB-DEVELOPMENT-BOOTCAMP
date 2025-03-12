0804d1464d3c59db4019fa4a984f73a6616c0c3d
BarnaliBhowmik <cocoospeak@gmail.com>
2025-03-12 22:40:16 +0530

    commit

--- 12.1 JAVASCRIPT/index.js
+++ 12.1 JAVASCRIPT/index.js
@@ -171,3 +171,15 @@ function getMilk(money) {
 }
 
 getMilk(5);
+
+//12. Life in weeks
+
+function lifeInWeeks(age) {
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+
+    console.log("You have " + days + + "days, " + weeks + "weeks, and" + months + "months left.");
+}
+lifeInWeeks(12);
