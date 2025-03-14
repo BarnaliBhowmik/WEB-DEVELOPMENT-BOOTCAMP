02f4c50a3db27db72ce0a6600afcf59207b01838
BarnaliBhowmik <cocoospeak@gmail.com>
2025-03-14 19:59:43 +0530

    commit

--- 12.1 JAVASCRIPT/index.js
+++ 12.1 JAVASCRIPT/index.js
@@ -271,3 +271,19 @@ if(loveScore > 70) {
 } else {
     alert("Your love score is " + loveScore + "%");
 }
+
+// 16. BMI Calculator Advanced (IF/ELSE)
+
+function bmiCalculator (weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    if (bmi < 24.9) {
+        if (bmi < 18.5) {
+            return ("Your BMI is " + bmi + ", so you are underweight.");
+        } else {
+            return ("Your BMI is " + bmi + ", so you have a normal weight.");
+        }
+    }
+    if ( bmi > 24.9) {
+        return ("Your BMI is " + bmi + ", so you are overweight.");
+    }
+}
