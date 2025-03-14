a03ef39d7ad7c564541042f1d2c2a0842fcfb206
BarnaliBhowmik <cocoospeak@gmail.com>
2025-03-14 16:01:37 +0530

    commit

--- 12.1 JAVASCRIPT/index.js
+++ 12.1 JAVASCRIPT/index.js
@@ -250,3 +250,11 @@ function bmiCalculator(weight, height) {
 }
 var bmi = bmiCalculator(65, 1.0);
 console.log(bmi);
+
+// 14. LOVE CALCULATOR
+
+prompt("what is your name?");
+prompt("What is their name?");
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your love score is " + loveScore + "%");
